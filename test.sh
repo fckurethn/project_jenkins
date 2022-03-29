e79b73451841c0839d8563b5e6cd7c91d9d3ac9e
Mykhailo Babych <fckurethn@gmail.com>
2022-03-29 20:20:19 +0300

    changed versioning

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-a=`sudo docker ps | grep fckurethn/my-flask-app | awk '{print $1}'`; [ "$a" != "" ] && sudo docker stop $a || echo 'There is no running container, go further'
+a=`docker ps | grep fckurethn/my-flask-app | awk '{print $1}'`; [ "$a" != "" ] && docker stop $a || echo 'There is no running container, go further'
