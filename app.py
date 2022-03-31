56139467e287fb6a2c531914e4adf463245100c3
Mykhailo Babych <fckurethn@gmail.com>
2022-03-31 22:43:50 +0300

    Updated main page

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 import random
 app = Flask(__name__)
-# list of cat images
+# list of pepe images
 images = [
-"https://media.istockphoto.com/photos/grey-cat-lying-on-bed-picture-id1082353380?s=612x612",
-"https://media.istockphoto.com/photos/mink-bengal-relaxing-outdoor-picture-id1019867458?s=612x612",
-"https://cdn.pixabay.com/photo/2014/04/05/12/24/cat-316985_960_720.jpg",
-"https://cdn.pixabay.com/photo/2017/11/23/15/34/kitten-2973049_960_720.jpg",
-"https://cdn.pixabay.com/photo/2015/06/09/09/35/animal-children-803123_960_720.jpg",
-"https://cdn.pixabay.com/photo/2017/06/29/10/22/folded-ears-2453763_960_720.jpg"]
+"https://pbs.twimg.com/profile_images/1285384386771394560/1kSxAdMB_400x400.jpg",
+"https://i.pinimg.com/474x/b4/f1/f1/b4f1f19330a0542e69f1ea8c92ced4fc.jpg",
+"https://img-07.stickers.cloud/packs/6971e3d5-efe4-4893-9938-aa9c5b5174fc/webp/6dd14ce6-87fe-40c6-baf7-fed843db5e11.webp",
+"https://64.media.tumblr.com/1b00fab3be8bee554173e51f570c064b/tumblr_otyc3wq9FK1w7964eo1_500.jpg",
+"https://stickerly.pstatic.net/sticker_pack/V3UsPH6pEtk8qTcLjMqdQ/F4MF00/33/f09686fb-1d57-4571-bac1-c27eed034321.png",
+"https://lh3.googleusercontent.com/4ethcI-9Xyhd7yXxDthP3AfRcgQu-FkrAspVivZUrt_6IBOtaBWJwB530xyzJNyO1wI"]
 @app.route('/')
 def index():
   url = random.choice(images)
